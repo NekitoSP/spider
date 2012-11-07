749d67807768d1793d1a118d01a3ad286333e679
Salavat Khanov <salavat@khanov.com>
2012-11-08 00:30:32 +0600

    Adding "Read more" javascript function, settings for the All Jobs page

---
+++ spider.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	var collapsedSize = '90px'; // how many pixs to show
+	$(".spider-item-description").each(function() {
+	    var h = this.scrollHeight;
+	    //console.log(h);
+	    var div = $(this);
+	    if (h > 30) {
+	        div.css('height', collapsedSize);
+	        div.after('<a id="more" class="item" href="#">Read more</a><br/>');
+	        var link = div.next();
+	        link.click(function(e) {
+	            e.stopPropagation(); // to determine if this method was ever called (on that event object)
+	
+	            if (link.text() != 'Collapse') {
+	                link.text('Collapse');
+	                div.animate({
+	                    'height': h
+	                });
+	
+	            } else {
+	                div.animate({
+	                    'height': collapsedSize
+	                });
+	                link.text('Read more');
+	            }
+	        });
+	    }
+	});
+});
