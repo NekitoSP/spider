6bbf3b975ea0a5cd3a75cdd0104b3a9a7e4dc202
NekitoSP <nekitosp@gmail.com>
2012-11-08 10:43:46 +0600

    parser  add constructor install  fix db's and add spider_active_spiders module  add cron

--- parser.php
+++ parser.php
@@ -40,12 +40,6 @@
 					} 
 				} 
 			} 
-			$ret = array( 
-				//'name'=>$name, 
-				//'text'=>$text, 
-				//'attributes'=>$attributes, 
-				//'children'=>$children 
-			); 
 			if (!$text==NULL) $ret['text'] = $text;
 			if (count($attributes)>=1) $ret['attributes'] = $attributes;
 			if (count($children)>=1) $ret['children'] = $children;
@@ -56,17 +50,18 @@
 	class HHParser extends SiteParser{
 		private $region;			//регион
 		private $professionalField;	//проф.область
-		public function HHParser(){
+
+		public function HHParser($params){
+			$region = $params->region;
+			$professionalField = $params->field;
 			//Регион: Башкортостан - 1347
 			//проф.область: IT - 1
-			
-			//Сортировка: по дате изменения - 2 //not for constructor
 		}
 
 		public function parse(){
-			//качаем страничку с hh.ru
+			//качаем страничку с hh.ru, пока без пагинации, последние 1000 записей
 			if( $curl = curl_init() ) {
-				curl_setopt($curl, CURLOPT_URL, 'http://api.hh.ru/1/xml/vacancy/search/?region=1347&order=2&field=1&items=1000');
+				curl_setopt($curl, CURLOPT_URL, 'http://api.hh.ru/1/xml/vacancy/search/?region=$region&order=2&field=$professionalField&items=1000');
 				curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
 				$out = curl_exec($curl);
 				//echo $out;
@@ -83,71 +78,94 @@
 
 			//цикл по скачанным вакансиям
 			foreach($parsedArr["children"]["vacancies"][0]["children"]["vacancy"] as $k => $val){
-				$jID = $val["attributes"]["id"];
+				$jobId = $val["attributes"]["id"];
+
+				$jobCompanyId = 0;
+				if (isset($val["children"]["employer"][0]["attributes"]["id"])){
+					$jobCompanyId  = $val["children"]["employer"][0]["attributes"]["id"];
+				}
+
+				//скачаем описание вакансии
+				unset($out);
 				if( $curl = curl_init() ) {
 					curl_setopt($curl, CURLOPT_URL, 'http://api.hh.ru/1/xml/vacancy/'.$jID.'/');
 					curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
-					$out2 = curl_exec($curl);
+					$out = curl_exec($curl);
 					curl_close($curl);
 				}
-				$doc = new SimpleXMLElement($out2);
+				$doc = new SimpleXMLElement($out);
 				$jobPage = parent::xmlObjToArr($doc);
+
+
+				//скачиваем страничку о компании, если надо
+				if( $curl = curl_init() ) {
+					curl_setopt($curl, CURLOPT_URL, 'http://api.hh.ru/1/xml/vacancy/'.$jID.'/');
+					curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+					$out = curl_exec($curl);
+					curl_close($curl);
+				}
 
 				$jobDescription = '';
 				if (isset($jobPage["children"]["description"][0]["text"])){
 					$jobDescription = $jobPage["children"]["description"][0]["text"];
 				}
-				
-				//$jLINK = $val["children"]["link"][1]["attributes"]["href"];
-				$jobCaption = $val["children"]["name"][0]["text"];
-				
-				$jCOMPID = 0;
-				if (isset($val["children"]["employer"][0]["attributes"]["id"])){
-					$jCOMPID  = $val["children"]["employer"][0]["attributes"]["id"];
+
+				$jobCaption = '';
+				if (isset($val["children"]["name"][0]["text"])){
+					$jobCaption = $val["children"]["name"][0]["text"];
 				}
-				$jUPDATETIME = $val["children"]["update"][0]["attributes"]["timestamp"];
-				
-				$jWAGEFROM = 0;
+
+				$jobLastUpdateTime = 0;
+				if (isset($val["children"]["update"][0]["attributes"]["timestamp"])){
+					$jobLastUpdateTime = $val["children"]["update"][0]["attributes"]["timestamp"];
+				}
+
+				$jobWageFrom = 0;
 				if (isset( $val["children"]["salary"][0]["children"]["from"][0]["text"] )){
-					$jWAGEFROM = $val["children"]["salary"][0]["children"]["from"][0]["text"];
+					$jobWageFrom = $val["children"]["salary"][0]["children"]["from"][0]["text"];
 				}
-				
-				$jWAGETO = 0;
+
+				$jobWageTo = 0;
 				if (isset( $val["children"]["salary"][0]["children"]["to"][0]["text"] )){
-					$jWAGETO = $val["children"]["salary"][0]["children"]["to"][0]["text"];
+					$jobWageTo = $val["children"]["salary"][0]["children"]["to"][0]["text"];
 				}
-				
-				$jWAGECURRENCY = "N/A";
+
+				$jobWageCurrency = "N/A";
 				if (isset( $val["children"]["salary"][0]["children"]["currency"][0]["text"] )){
-					$jWAGECURRENCY = $val["children"]["salary"][0]["children"]["currency"][0]["text"];
+					$jobWageCurrency = $val["children"]["salary"][0]["children"]["currency"][0]["text"];
 				}
 
 				//вакансия "свежа", добавим в базу или обновим если есть
-				if (!isset($accLast["updated"]) || $accLast["updated"]<$jUPDATETIME){
-					$res = db_query('SELECT * FROM {spider_joblist} WHERE id = %d',$jID);
+				if (!isset($accLast["updated"]) || $accLast["updated"]<$jobLastUpdateTime){
+					$res = db_query('SELECT * FROM {spider_joblist} WHERE id = %d',$jobId);
 					$arr = db_fetch_array($res);
 					//if not exist = INSERT NEW, else = update existing
 					if ($arr == FALSE){
-						db_query('INSERT INTO {spider_joblist} (`id`, `name`, `compid`, `updated`, `wagefrom`, `wageto`, `wagecurrency`, `jobdescription`) VALUES (%d, \'%s\', %d, %d, %d, %d, \'%s\', \'%s\');',$jID, $jobCaption, $jCOMPID, $jUPDATETIME, $jWAGEFROM, $jWAGETO, $jWAGECURRENCY, $jobDescription);
+						db_query('INSERT INTO {spider_joblist} (`id`, `name`, `compid`, `updated`, `wagefrom`, `wageto`, `wagecurrency`, `jobdescription`) VALUES (%d, \'%s\', %d, %d, %d, %d, \'%s\', \'%s\');',$jobId, $jobCaption, $jobCompanyId, $jobLastUpdateTime, $jobWageFrom, $jobWageTo, $jobWageCurrency, $jobDescription);
 					} else {
-						db_query("UPDATE {spider_joblist} SET `name`='$jobCaption', `compid`='$jCOMPID', `updated`='$jUPDATETIME', `wagefrom`='$jWAGEFROM', `wageto`='$jWAGETO', `wagecurrency`='$jWAGECURRENCY', `jobdescription`='$jobDescription' WHERE `id`='$jID' ");
+						db_query("UPDATE {spider_joblist} SET `name`='$jobCaption', `compid`='$jobCompanyId', `updated`='$jobLastUpdateTime', `wagefrom`='$jobWageFrom', `wageto`='$jobWageTo', `wagecurrency`='$jobWageCurrency', `jobdescription`='$jobDescription' WHERE `id`='$jobId' ");
 					}
 				}
 
 				//var_dump($accLast["updated"]);
-				$retnArr[$k]["id"] = $jID;
+				/*
+				$retnArr[$k]["id"] = $jobId;
 				$retnArr[$k]["text"] = $jobCaption;
-				$retnArr[$k]["compid"] = $jCOMPID;
-				$retnArr[$k]["update"] = $jUPDATETIME;
-				$retnArr[$k]["wagefrom"] = $jWAGEFROM;
-				$retnArr[$k]["wageto"] = $jWAGETO;
-				$retnArr[$k]["wagecurrency"] = $jWAGECURRENCY;
+				$retnArr[$k]["compid"] = $jobCompanyId;
+				$retnArr[$k]["update"] = $jobLastUpdateTime;
+				$retnArr[$k]["wagefrom"] = $jobWageFrom;
+				$retnArr[$k]["wageto"] = $jobWageTo;
+				$retnArr[$k]["wagecurrency"] = $jobWageCurrency;
 				$retnArr[$k]["jobdescription"] = $jobDescription;
+				*/
 			}
-			//var_dump($retnArr);
-			//STUPID COMMENT
 		}
 	}
 	//$p = new HHParser();
 	//$p->parse();
+
+	/*
+	TODO:
+		функция для получения параметров и их читабельных вариантов каждого парсера
+	*/
 ?>
